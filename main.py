9fdbc6d33d15b07220db0eba68dd479f7992d3ea
Vital.Guilherme <guilhermev.j.santos@gmail.com>
2025-10-14 19:32:31 -0300

    Adicionando ordenacao no endpoint do /livros

--- main.py
+++ main.py
@@ -66,12 +66,14 @@ def get_livro(page: int = 1, limit: int = 10, credentials: HTTPBasicCredentials 
     if not meu_livro:
         return {"message": "Não existe nenhum livro."}
     
+    livros_ordenados = sorted(meu_livro.items(), key=lambda x: x[0])
+
     start = (page - 1) * limit
     end = start + limit
 
     livros_paginados = [
         {"id": id_livro, "nome_livro": livro_data["nome_livro"], "autor_livro": livro_data["autor_livro"], "ano_livro": livro_data["ano_livro"]}
-        for id_livro, livro_data in list(meu_livro.items())[start:end]
+        for id_livro, livro_data in livros_ordenados[start:end]
     ]
 
     return {
